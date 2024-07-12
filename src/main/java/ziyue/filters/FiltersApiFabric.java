7bcdb551bc34f0a0d72b44473e95192748e1ee9a
子悦解说 <ziyuecommentary@gmail.com>
2024-07-12 13:51:17 +0800

    Update FiltersApiFabric.java

--- src/main/java/ziyue/filters/FiltersApiFabric.java
+++ src/main/java/ziyue/filters/FiltersApiFabric.java
@@ -11,16 +11,5 @@ public class FiltersApiFabric implements ClientModInitializer
     @Override
     public void onInitializeClient() {
         FiltersApi.LOGGER.info("Filters API initialized!");
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("1"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("2"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("3"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("4"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("5"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("6"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("7"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("8"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("9"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerFilter(ItemGroup.COMBAT, new LiteralText("10"), () -> new ItemStack(Items.GLOW_ITEM_FRAME));
-        FilterBuilder.registerUncategorizedItemsFilter(ItemGroup.COMBAT);
     }
 }
